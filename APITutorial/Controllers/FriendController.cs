36eb1aefc6aca86ad95cde1f16526842f992776d
Matheus Simões da Silva <matheussimoesdasilva2002@gmail.com>
2023-11-08 09:48:36 -0300

    Initial commit

---
+++ APITutorial/Controllers/FriendController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace APITutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FriendController : ControllerBase
+    {
+        private List<Friend> friends = new List<Friend>();
+
+        public FriendController()
+        {
+            friends.Add(new Friend(1, "Kindson", "Munonye", "Budapest", DateTime.Today));
+            friends.Add(new Friend(2, "Oleander", "Yuba", "Nigeria", DateTime.Today));
+            friends.Add(new Friend(3, "Saffron", "Lawrence", "Lagos", DateTime.Today));
+            friends.Add(new Friend(4, "Jadon", "Munonye", "Asaba", DateTime.Today));
+        }
+
+        // GET: api/Friend
+        [HttpGet]
+        public List<Friend> Get()
+        {
+            return friends;
+        }
+
+        // GET api/Friend/5
+        [HttpGet("{id}", Name = "Get")]
+        public Friend Get(int id)
+        {
+            Friend friend = friends.Find(f => f.id == id);
+            return friend;
+        }
+
+        // POST api/Friend
+        [HttpPost]
+        public List<Friend> Post([FromBody] Friend friend)
+        {
+            friends.Add(friend);
+            return friends;
+        }
+
+        // PUT api/Friend/5
+        [HttpPut("{id}")]
+        public List<Friend> Put(int id, [FromBody] Friend friend)
+        {
+            Friend friendToUpdate = friends.Find(f => f.id == id);
+            int index = friends.IndexOf(friendToUpdate);
+
+            friends[index].firstname = friend.firstname;
+            friends[index].lastname = friend.lastname;
+            friends[index].location = friend.location;
+            friends[index].dateOfHire = friend.dateOfHire;
+
+            return friends;
+        }
+        
+
+        // DELETE api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public List<Friend> Delete(int id)
+        {
+            Friend friend = friends.Find(f => f.id == id);
+            friends.Remove(friend);
+            return friends;
+        }
+    }
+}
---
+++ APITutorial/Friend.cs
@@ -0,0 +1,23 @@
+﻿namespace APITutorial
+{
+    public class Friend
+    {
+        public int id { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string location { get; set; }
+        public DateTime dateOfHire { get; set; }
+
+        public Friend()
+        {
+        }
+
+        public Friend(int id, string firstname, string lastname, string location, DateTime dateOfHire)
+        {
+            this.id = id;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.dateOfHire = dateOfHire;
+        }
+    }
+}
